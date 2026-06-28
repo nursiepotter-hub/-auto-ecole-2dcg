7037e4562866a55b25d635719cc4923f28a273ef
Astou Kamara <astoukamara@Astous-MacBook-Air.local>
2026-06-28 20:23:19 +0200

    Add Google Analytics 4 tracking (react-ga4)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import ReactGA from 'react-ga4'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import WhatsAppButton from '@/components/WhatsAppButton'
@@ -10,9 +12,18 @@ import Avis from '@/pages/Avis'
 import Galerie from '@/pages/Galerie'
 import Inscription from '@/pages/Inscription'
 
+function AnalyticsTracker() {
+  const location = useLocation()
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname + location.search })
+  }, [location])
+  return null
+}
+
 export default function App() {
   return (
     <BrowserRouter>
+      <AnalyticsTracker />
       <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-1">
